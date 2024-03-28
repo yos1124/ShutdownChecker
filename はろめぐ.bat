62fcedb8194163fd94729f82995d415a7d54311d
rimo1124 <you-2904@i.softbank.jp>
2024-03-28 16:32:14 +0900

    commit

---
+++ はろめぐ.bat
@@ -0,0 +1,29 @@
+@echo off
+
+rem 各種パス生成
+set path=%~p0
+set vbsFile=%path%msgbox.vbs
+set wavPath=%path%shutdown.wav
+set playTime=5000
+
+rem 既にvbsが存在した場合に削除する
+if exist "%vbsFile%" (
+	del /f "%vbsFile%"
+)
+
+rem vbs生成
+echo Dim answer >> %vbsFile%
+echo answer = msgbox("シャットダウンする？",vbYesNo + vbInformation,"藤島慈") >> "%vbsFile%"
+echo WScript.Quit(answer) >> "%vbsFile%"
+
+rem vbs実行
+start /b %wavPath%
+"%vbsFile%"
+
+rem vbs実行結果確認
+if %errorlevel% == 1 (
+	shutdown.exe /s /t 0
+)
+
+rem vbs削除
+del /f "%vbsFile%"
